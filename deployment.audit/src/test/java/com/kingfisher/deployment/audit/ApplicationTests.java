6ab3df9bf7978bdc8782ed856af55b8be10cbd6e
mandas04 <mandas04@GBKTFDC013X0004.kits.kfplc.com>
2019-01-15 21:05:20 +0530

    JaCoCo integration

--- deployment.audit/src/test/java/com/kingfisher/deployment/audit/ApplicationTests.java
+++ deployment.audit/src/test/java/com/kingfisher/deployment/audit/ApplicationTests.java
@@ -1,5 +1,7 @@
 package com.kingfisher.deployment.audit;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +13,7 @@ public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		assertTrue(true);
 	}
 
 }
